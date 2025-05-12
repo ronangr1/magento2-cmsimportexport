94470e7a0a78d1e2dd9d030b2c086647cb6420b5
Ronan Guérin <guerin.ronan@proton.me>
2025-05-12 09:11:16 +0200

    Update Exporter.php

--- Model/Exporter.php
+++ Model/Exporter.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © Ronangr1, All rights reserved.
+ * See LICENSE bundled with this library for license details.
+ */
 declare(strict_types=1);
 
 namespace Ronangr1\CmsImportExport\Model;
