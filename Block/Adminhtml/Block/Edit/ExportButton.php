dd63ab4c7a90e9c783104adbc11354493dbcc832
Ronan Guérin <ronan.guerin@humansconnexion.com>
2025-05-13 12:04:42 +0200

    add acl

--- Block/Adminhtml/Block/Edit/ExportButton.php
+++ Block/Adminhtml/Block/Edit/ExportButton.php
@@ -7,20 +7,27 @@ declare(strict_types=1);
 
 namespace Ronangr1\CmsImportExport\Block\Adminhtml\Block\Edit;
 
+use Magento\Framework\AuthorizationInterface;
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 use Magento\Framework\View\Element\UiComponent\Context;
 
 class ExportButton implements ButtonProviderInterface
 {
+    private const ACL_RESOURCE_EXPORT = 'Ronangr1_CmsImportExport::export';
 
     public function __construct(
-        private readonly Context $context
+        private readonly Context $context,
+        private readonly AuthorizationInterface $authorization
     )
     {
     }
 
     public function getButtonData(): array
     {
+        if (!$this->isAllowed()) {
+            return [];
+        }
+
         $blockId = (int) $this->context->getRequestParam('block_id');
 
         return [
@@ -37,5 +44,10 @@ class ExportButton implements ButtonProviderInterface
             'cmsimportexport/block/export',
             ['id' => $blockId]
         );
+    }
+
+    public function isAllowed(): bool
+    {
+        return $this->authorization->isAllowed(self::ACL_RESOURCE_EXPORT);
     }
 }
--- Block/Adminhtml/Page/Edit/ExportButton.php
+++ Block/Adminhtml/Page/Edit/ExportButton.php
@@ -7,19 +7,27 @@ declare(strict_types=1);
 
 namespace Ronangr1\CmsImportExport\Block\Adminhtml\Page\Edit;
 
+use Magento\Framework\AuthorizationInterface;
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 use Magento\Framework\View\Element\UiComponent\Context;
 
 class ExportButton implements ButtonProviderInterface
 {
+    private const ACL_RESOURCE_EXPORT = 'Ronangr1_CmsImportExport::export';
+
     public function __construct(
-        private readonly Context $context
+        private readonly Context $context,
+        private readonly AuthorizationInterface $authorization
     )
     {
     }
 
     public function getButtonData(): array
     {
+        if (!$this->isAllowed()) {
+            return [];
+        }
+
         $pageId = (int) $this->context->getRequestParam('page_id');
 
         return [
@@ -36,5 +44,10 @@ class ExportButton implements ButtonProviderInterface
             'cmsimportexport/page/export',
             ['id' => $pageId]
         );
+    }
+
+    public function isAllowed(): bool
+    {
+        return $this->authorization->isAllowed(self::ACL_RESOURCE_EXPORT);
     }
 }
